1e8d1865aee277fbebe4bd0b111d8ba2bf51bb3b
p4c4t <vsmorodov+github@protonmail.com>
2023-02-09 01:08:49 +0300

    Fixed synchronisation issues with waitgroups. Implemented RandFill (as a unary opeation). Added more tests.

--- numgo.go
+++ numgo.go
@@ -1,5 +1,10 @@
 package numgo
 
+import (
+	"math/rand"
+	"sync"
+)
+
 func assert(b bool, errmsg string) {
 	if !b {
 		panic(errmsg)
@@ -18,9 +23,18 @@ func Make(length uint) Int64Array {
 	}
 }
 
+func Print(a Int64Array) {
+	for _, x := range a.data {
+		print(x, " ")
+	}
+	println()
+}
+
 type BinaryByElemFunc func(int64, int64) int64
 
-func BinOperateSlow(res, a, b *[]int64, start, end uint, op BinaryByElemFunc) {
+func BinOperateSlow(res, a, b *[]int64, start, end uint, op BinaryByElemFunc, wg *sync.WaitGroup) {
+	defer wg.Done()
+
 	for i := uint(start); i < uint(end); i++ {
 		(*res)[i] = op((*a)[i], (*b)[i])
 	}
@@ -37,14 +51,18 @@ func BinOperateParallel(a, b *Int64Array, op BinaryByElemFunc, threads uint) *In
 	)
 	n := a.length
 	res := Make(n)
+	var wg sync.WaitGroup
 	for i := uint(0); i < threads; i++ {
+		wg.Add(1)
 		go BinOperateSlow(
 			&res.data,
 			&a.data, &b.data,
 			(n*i)/threads, (n*(i+1))/threads,
 			op,
+			&wg,
 		)
 	}
+	wg.Wait()
 	return &res
 }
 
@@ -58,11 +76,63 @@ func Add(a, b *Int64Array) *Int64Array {
 	)
 }
 
-func Dot(a, b *Int64Array) *Int64Array {
+func Mult(a, b *Int64Array) *Int64Array {
 	return BinOperateParallel(
 		a, b,
 		func(a, b int64) int64 {
 			return a * b
+		},
+		20,
+	)
+}
+
+type UnaryByElemFunc func(int64) int64
+
+func UnOperateSlow(res, a *[]int64, start, end uint, op UnaryByElemFunc, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for i := uint(start); i < uint(end); i++ {
+		(*res)[i] = op((*a)[i])
+	}
+}
+
+func UnOperateParallel(a *Int64Array, op UnaryByElemFunc, threads uint) *Int64Array {
+	assert(
+		threads != 0,
+		"we can't just do nothing!",
+	)
+	n := a.length
+	res := Make(n)
+	var wg sync.WaitGroup
+	for i := uint(0); i < threads; i++ {
+		wg.Add(1)
+		go UnOperateSlow(
+			&res.data,
+			&a.data,
+			(n*i)/threads, (n*(i+1))/threads,
+			op,
+			&wg,
+		)
+	}
+	wg.Wait()
+	return &res
+}
+
+func RandFill(a *Int64Array) *Int64Array {
+	return UnOperateParallel(
+		a,
+		func(x int64) int64 {
+			return rand.Int63()
+		},
+		20,
+	)
+}
+
+func SmRandFill(a *Int64Array) *Int64Array {
+	return UnOperateParallel(
+		a,
+		func(x int64) int64 {
+			return rand.Int63n(10)
 		},
 		20,
 	)
--- numgo_test.go
+++ numgo_test.go
@@ -1,10 +1,34 @@
-package numgo
+package numgo_test
 
 import (
+	"numgo"
 	"testing"
 )
 
+func TestRand(test *testing.T) {
+	a, b := numgo.Make(5), numgo.Make(5)
+	c, d := numgo.SmRandFill(&a), numgo.RandFill(&b)
+	numgo.Print(*c)
+	numgo.Print(*d)
+}
+
+func TestAdd(test *testing.T) {
+	a, b := numgo.Make(5), numgo.Make(5)
+	c, d := numgo.SmRandFill(&a), numgo.SmRandFill(&b)
+	numgo.Print(*c)
+	numgo.Print(*d)
+	numgo.Print(*numgo.Add(c, d))
+}
+
+func TestMult(test *testing.T) {
+	a, b := numgo.Make(5), numgo.Make(5)
+	c, d := numgo.SmRandFill(&a), numgo.SmRandFill(&b)
+	numgo.Print(*c)
+	numgo.Print(*d)
+	numgo.Print(*numgo.Mult(c, d))
+}
+
 func BenchmarkAddParallel20(bench *testing.B) {
-	a, b := Make(1e8), Make(1e8)
-	Add(&a, &b)
+	a, b := numgo.Make(1e8), numgo.Make(1e8)
+	numgo.Add(&a, &b)
 }
